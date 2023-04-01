c2e893418a8d1a43ffc157e9c4f6d99271579e2c
rksssgthjuk7j66h <95147076+rksssgthjuk7j66h@users.noreply.github.com>
2023-04-01 10:50:29 +0300

    Create Yeah.lua

---
+++ Yeah.lua
@@ -0,0 +1,3 @@
+--xonae stop snoopying  aimhook is shit storm you can lick my ball sack xonae if you patch i will make a ai deepfake of your voice saying the n word
+repeat wait() until game:IsLoaded()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/dca3e69649ed196af0ac6577f743a0ae.lua"))()
